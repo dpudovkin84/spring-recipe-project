f699e0f6b6346f6048160ab51c431470744fffb5
dpudovkin <dpudovkin@sbrf.ru>
2022-03-30 15:52:59 +0300

    add exception handler

--- src/main/java/edu/myspring/recipe/controllers/RecipeController.java
+++ src/main/java/edu/myspring/recipe/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package edu.myspring.recipe.controllers;
 
 import edu.myspring.recipe.commands.RecipeCommand;
+import edu.myspring.recipe.exceptions.NotFoundException;
 import edu.myspring.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -50,6 +53,15 @@ public class RecipeController {
         log.debug("Deleting id:"+id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
-
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+        ModelAndView modelAndView =new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
+    }
+
 }
